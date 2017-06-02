4f8e929f8748a32aa556e2f56e0d6a07232c8cac
Zach Gulde <zachgulde@gmail.com>
2017-06-02 13:57:04 -0500

    use npm for project managment
    - bump gitbook version
    - install gitbook locally instead of globally
    - use npm to serve and build the project
    - specify gitbook version in package.json, instead of book.json

--- codeship.sh
+++ codeship.sh
@@ -3,10 +3,7 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 
-nvm install 5.7
+nvm install 7.7.1
 
-# Current version (2.1.1) has a bug when installing dependencies.
-npm install -g gitbook-cli@2.0.1
-
-gitbook install book
-gitbook build book build
+npm install
+npm run build
